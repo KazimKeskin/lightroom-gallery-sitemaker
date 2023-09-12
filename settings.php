13e3512e6eb1b95a76ed6c35066753afb210b2f0
KazimKeskin <115115454+KazimKeskin@users.noreply.github.com>
2023-09-12 20:56:52 +0100

    Update settings.php

--- settings.php
+++ settings.php
@@ -8,12 +8,13 @@ $baseurl = "";
 $webgallerypath = ""; // the url path to where your website gallery will be
 
 $addstylesheet = 1;
-$stylesheet = "assets/css/actions.css";
+$stylesheet = "assets/css/sitemaker.css";
 
 $addscript = 1;
-$script = "assets/js/actions.js";
+$script = "assets/js/sitemaker.js";
 
 $editpaginationsize = 1;
+$paginationsize = "350";
 
 $addtitle = 1;
 
